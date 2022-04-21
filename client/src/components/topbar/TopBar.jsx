bd835d7e5ecd69c566868474728f3531e994e68b
Blesson Biju Abraham <bbabrahambb@gmail.com>
2022-04-21 11:48:01 +0530

    Backend1

---
+++ client/src/components/topbar/TopBar.jsx
@@ -0,0 +1,62 @@
+import { Link } from 'react-router-dom';
+import './topbar.css'
+
+export default function TopBar() {
+    const user = true;
+        return (
+        <div className='top'>
+            <div className="topLeft">
+                <i className="topIcon fa-brands fa-twitter"></i>
+                <i className="topIcon fa-brands fa-facebook"></i>
+                <i className="topIcon fa-brands fa-pinterest"></i>
+                <i className="topIcon fa-brands fa-instagram"></i>
+            </div>
+            <div className="topCenter">
+                <ul className="topList">
+                    <li className='topListItem' onClick={() => window.location.reload()}>
+                    <Link to="/" className='link'>HOME</Link>    
+                    {/* <Route render={(props) => <Link to='/' className='link'>HOME</Link>}/> */}
+                    </li>
+                    <li className='topListItem' onClick={() => window.location.reload()}> 
+                    <Link  to='/' className='link'>CONTACT</Link>
+                    
+                    </li>
+                    <li className='topListItem' onClick={() => window.location.reload()}> 
+                    <Link  to='/write' className='link'>WRITE</Link> 
+                    
+                    </li>
+                    <li className='topListItem' onClick={() => window.location.reload()}> 
+                    <Link  to='/' className='link'>ABOUT</Link> 
+
+                    </li>
+                    <li className='topListItem'>
+                        {user && "LOGOUT"}    
+                    </li>
+                    
+                </ul>
+            </div>
+            <div className="topRight">
+                {
+                    user ? (
+                        <img className='topImg' src='http://images.vogue.it/users/media/a/amirhossein.png?v=1467238729' alt="img" />
+                    ) : (
+                        <ul className='topList'>
+                            <li className='topListItem' onClick={() => window.location.reload()}>
+                                <Link className='link' to='/login'>LOGIN</Link>
+                            </li>
+                            <li className='topListItem' onClick={() => window.location.reload()}>
+                                <Link className='link' to='/register'>REGISTER</Link>
+                            </li>
+                        </ul>
+                                // <>
+                                // <Link className='link' to='/register'>REGISTER</Link>
+                                // <Link className='link' to='/login'>LOGIN</Link>
+                                // </ul>
+                    )
+                }
+                <i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
+            </div>
+            
+        </div>
+    )
+}
