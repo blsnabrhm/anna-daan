adedd6dc55287518018123d31645b639d33a1b39
Chirag Ganguli <chiragganguli11@gmail.com>
2022-04-22 11:31:09 +0530

    Updated Context

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -4,7 +4,7 @@ const Post=require("../models/Post");
 
 
 //create post
-router.post("/:id", async (req,res)=>{
+router.post("/", async (req,res)=>{
       const newPost=new Post(req.body);
       try{
         const savedPost= await newPost.save();
--- client/src/pages/contact/Contact.jsx
+++ client/src/pages/contact/Contact.jsx
@@ -8,42 +8,43 @@ export default function Contact() {
 			Donation Form
 		</div>
 
-		<form>
+		<form action="mailto:chirag.ganguli2019@vitbhopal.ac.in" method="POST" enctype="text/plain">
+			On submitting, you will be redirected to the mail client, where you can just send the mail for approval.
 			<div class="form_wrap">
                 <div className="input_wrap">
 					<label for="fname">First Name</label>
-					<input type="text" id="email"/>
+					<input type="text" id="fname" name="fname"/>
 				</div>
 				<div className="input_wrap">
 					<label for="lname">Last Name</label>
-					<input type="text" id="email"/>
+					<input type="text" id="lname" name="lname"/>
 				</div>
                 <div >
 
                 </div>
 			
                 <div className="input_wrap">
-					<label for="fname">Food Name</label>
-					<input type="text" id="email"/>
+					<label for="foodname">Food Name</label>
+					<input type="text" id="foodname" name="foodname"/>
 				</div>
                 <div className="input_wrap">
 					<label>Food Type</label>
 					<ul>
 						<li>
 							<label className="radio_wrap">
-								<input type="radio" name="gender" value="male" className="input_radio" checked/>
+								<input type="radio" name="state" value="solid" className="input_radio" checked/>
 								<span>Solid</span>
 							</label>
 						</li>
 						<li>
 							<label className="radio_wrap">
-								<input type="radio" name="gender" value="female" className="input_radio"/>
+								<input type="radio" name="state" value="liquid" className="input_radio"/>
 								<span>Liquid</span>
 							</label>
 						</li>
                         <li>
 							<label className="radio_wrap">
-								<input type="radio" name="gender" value="male" className="input_radio" checked/>
+								<input type="radio" name="state" value="semi-solid" className="input_radio" checked/>
 								<span>Semi-Solid</span>
 							</label>
 						</li>
@@ -52,47 +53,39 @@ export default function Contact() {
                 <div className="input_wrap">
                     <label className='edi' for="edible">Edible</label>
                     <div className='edible'>
-                    <input className='edible' type="radio" id="y" name='edible'/>
+                    <input className='edible' type="radio" id="y" name='edible' value="isEdible"/>
                     <label className='edible' for="edible">Yes  </label>
-                    <input className='edible' type="radio" id="n" name='edible'/>
+                    <input className='edible' type="radio" id="n" name='edible' value="isNotEdible"/>
                     <label className='edible' for="edible">No  </label>
                     </div>
                 </div>
                 <div className='input_wrap'>
-                    <label for="dot">Date of Cooking</label>
-					<input type="datetime-local" id="dot" class="ta"/>
-                </div>
-                <div className='input_wrap'>
-                    <label for="dot">Time of Cooking</label>
-					<input type="time" id="dot" class="ta"/>
+                    <label for="dot">Date and Time of Cooking</label>
+					<input type="datetime-local" id="dot" class="ta" name="cooking"/>
                 </div>
                 <div className='input_wrap'>
                     <label for="quant">Quantity</label>
-					<input type="number" id="quant" placeholder="in Kgs"/>
+					<input type="number" id="quant" placeholder="in Kgs" name="quantity"/>
                 </div>
                 <div className='input_wrap'>
                     <label for="loc">Pickup Location:</label>
-					<input type="text" id="loc" class="ta"/>
+					<input type="text" id="loc" class="ta" name="pickup from"/>
                 </div>
                 <div className='input_wrap'>
-                    <label for="pick">Date of PickUp</label>
-					<input type="datetime-local" id="pick"/>
-                </div>
-                <div className='input_wrap'>
-                    <label for="pick">Date of PickUp</label>
-					<input type="time" id="pick"/>
+                    <label for="pick">Date and Time of PickUp</label>
+					<input type="datetime-local" id="pick" name="data time pickup"/>
                 </div>
 				<div className="input_wrap">
 					<label for="city">Phone No</label>
-					<input type="tel" id="city" placeholder="+91"/>
+					<input type="tel" id="city" placeholder="+91" name="phone"/>
 				</div>
 				<div className="input_wrap">
 					<label for="country">Email</label>
-					<input type="text" id="country"/>
+					<input type="text" id="country" name="mail"/>
 				</div>
                 <div className='input_wrap'>
                     <label>Additional Information(if Any)</label>
-					<textarea for="ta"></textarea>
+					<textarea for="ta" name="addl info"></textarea>
                 </div>
 				<div className="input_wrap">
 					<input type="submit" value="Submit" className="submit_btn"/>
